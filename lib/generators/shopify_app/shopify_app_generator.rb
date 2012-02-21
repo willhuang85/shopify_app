84fc1af7860e8c7df985c9ddade9ad72a811bd6d
Warren Dunlop <warrendunlop@gmail.com>
2012-02-21 18:34:11 -0500

    Lock Less Rails Bootstrap to 2.0.4 (make sure docs and overrides are relevant for future use)

--- lib/generators/shopify_app/shopify_app_generator.rb
+++ lib/generators/shopify_app/shopify_app_generator.rb
@@ -33,7 +33,7 @@ class ShopifyAppGenerator < Rails::Generators::Base
   end
 
   def add_bootstrap_gem
-    insert_into_file "Gemfile", "\ngem 'less-rails-bootstrap'\n\n", :before => '# Gems used only for assets and not required'
+    insert_into_file "Gemfile", "\ngem 'less-rails-bootstrap', '~> 2.0.4'\n\n", :before => '# Gems used only for assets and not required'
   end
   
   def add_routes
